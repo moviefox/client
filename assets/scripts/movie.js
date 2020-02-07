fdbe16cc785ef1a45fea7540b971bf640b9a125e
Hansin Susatya <hansinsusatya@gmail.com>
2020-02-07 08:35:09 +0700

    Search Movie

--- assets/scripts/movie.js
+++ assets/scripts/movie.js
@@ -30,17 +30,71 @@ function signOut() {
   });
 }
 
+const hideWhenSearch = () => {
+  $('#movie-carousel').hide()
+  $('#popular-container').hide()
+}
+
+const showHomePage = () => {
+  $('#movie-carousel').show()
+  $('#popular-container').show()
+  $('#gsignin').hide()
+  $('#result-search-movie').hide()
+}
+
+const loginPage = () => {
+  $('#result-search-movie').hide()
+  hideWhenSearch()
+  $('#gsignin').show()
+}
+
+const renderSearchMovie = () => {
+  pageSearch = 1
+  hideWhenSearch()
+  let title = $('#query-search-movie').val()
+  title = '"' + title + '"'
+  $('span.findSearchTerm').text(title)
+  $('#result-search-movie').show()
+  searchMovie(title, pageSearch)
+}
+
 let page = 1
+let pageSearch = 1
 $(document).ready(() => {
+  $('#gsignin').hide()
+  $('#result-search-movie').hide()
   renderCarousel(page)
   $('#movie-carousel').carousel()
   getPopularMovies(page)
 
   $(window).on('scroll', () => {
-    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-      page = page + 1
-      getPopularMovies(page)
+    if ($('#popular-container').css('display') == 'block') {
+      if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+        page = page + 1
+        getPopularMovies(page)
+      }
     }
+  })
+
+  $('#btn-search-movie').on('click', (e) => {
+    e.preventDefault();
+    renderSearchMovie()
+  })
+
+  $('#query-search-movie').on('keypress', (e) => {
+    if (e.keyCode === 13) {
+      renderSearchMovie()
+    }
+  })
+
+  $('.navbar-brand').on('click', (e) => {
+    e.preventDefault()
+    showHomePage()
+  })
+
+  $('#navbar-login').on('click', (e) => {
+    e.preventDefault()
+    loginPage()
   })
 })
 
