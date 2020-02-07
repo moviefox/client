98acfa5ab8b25b10a6dd978377f44e4c18fe4f3d
Hansin Susatya <hansinsusatya@gmail.com>
2020-02-07 15:16:29 +0700

    Favourite Movie

--- assets/scripts/movie.js
+++ assets/scripts/movie.js
@@ -32,6 +32,7 @@ const hideWhenSearch = () => {
   $('#movie-carousel').hide()
   $('#popular-container').hide()
   $('#gsignin').hide()
+  $('#favourite-movies table').hide()
 }
 
 const showHomePage = () => {
@@ -39,12 +40,23 @@ const showHomePage = () => {
   $('#popular-container').show()
   $('#gsignin').hide()
   $('#result-search-movie').hide()
+  $('#movie-details').hide()
+  $('#favourite-movies table').hide()
+}
+
+const hideHomePage = () => {
+  $('#movie-carousel').hide()
+  $('#popular-container').hide()
+  $('#gsignin').hide()
+  $('#result-search-movie').hide()
+  $('#favourite-movies table').hide()
 }
 
 const loginPage = () => {
   $('#result-search-movie').hide()
   hideWhenSearch()
   $('#gsignin').show()
+  $('#movie-details').hide()
 }
 
 const renderSearchMovie = () => {
@@ -57,10 +69,110 @@ const renderSearchMovie = () => {
   searchMovie(title, pageSearch)
 }
 
+const getDetail = (movie) => {
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/movies/detail',
+    data: {
+      title: movie
+    }
+  })
+    .done(movie => {
+      hideHomePage()
+      $('#movie-details').show()
+      $('#movie-title').text(movie.Title)
+      $('#poster').html(`<img width="182" height="268" src="${movie.Poster}" alt="${movie.Title}">`)
+      $('#movie-year').text(`${movie.Year}`)
+      $('#movie-plot').text(`${movie.Plot}`)
+      if (localStorage.token) {
+        $('#login-first').hide()
+        $('#favourite-btn').show()
+        const data = []
+        data.push(movie)
+        $('#btn-favourite-movies').attr('onclick', 'favouriteMovie(' + JSON.stringify(data) + ')')
+      } else {
+        $('#login-first').show()
+        $('#favourite-btn').hide()
+      }
+    })
+    .fail(err => {
+
+    })
+}
+
+const favouriteMovie = (movie) => {
+  movie = movie[0]
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/movies',
+    headers: {
+      token: localStorage.token
+    },
+    data: {
+      title: movie.Title,
+      year: movie.Year,
+      plot: movie.Plot,
+      poster: movie.Poster
+    }
+  })
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+const deleteFavourites = (id) => {
+  $.ajax({
+    method: 'DELETE',
+    url: 'http://localhost:3000/movies/' + id,
+    headers: {
+      token: localStorage.token
+    }
+  })
+    .then(deleted => {
+      getFavouriteMovies()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+const getFavouriteMovies = () => {
+  $.ajax({
+    method: 'GET',
+    url: 'http://localhost:3000/users/movie',
+    headers: {
+      token: localStorage.token
+    }
+  })
+    .then(data => {
+      let contentHtml = ''
+      for (const movie of data[0].Movies) {
+        contentHtml += '<tr>'
+        contentHtml += '<td>' + movie.title + '</td>'
+        contentHtml += '<td>' + movie.year + '</td>'
+        contentHtml += '<td><img width="182" height="268" src="' + movie.poster + '" alt="' + movie.title + '"></td>'
+        contentHtml += '<td>' + movie.plot + '</td>'
+        contentHtml += '<td>'
+        contentHtml += '<a style="color:red;" class="delete-favourites link pointer" onclick="deleteFavourites(' + movie.id + ')"><i class="fa fa-trash" aria-hidden="true"></i></a>'
+        contentHtml += '</td>'
+        contentHtml += '</tr>'
+      }
+      $('#favourite-movies table').html(contentHtml)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
 let page = 1
 let pageSearch = 1
 $(document).ready(() => {
 
+  $('#favourite-movies table').hide()
+  $('#movie-details').hide()
   if (localStorage.token) {
     $('#navbarDropdown').show()
     $('#navbar-login').hide()
@@ -68,7 +180,6 @@ $(document).ready(() => {
     $('#navbarDropdown').hide()
     $('#navbar-login').show()
   }
-
 
   $('#gsignin').hide()
   $('#loader').hide()
@@ -117,7 +228,11 @@ $(document).ready(() => {
     $('#navbarDropdown').hide()
   })
 
-  $('#popular-container').on('click', '.movie-cart', () => {
+  $('#movie-favourites').on('click', (e) => {
+    hideHomePage()
+    $('#movie-details').hide()
+    $('#favourite-movies table').show()
+    getFavouriteMovies()
   })
 })
 
